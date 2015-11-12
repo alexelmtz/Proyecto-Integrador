f5e7fe1e94623ab1f67da88795b403dae204586a
Alejandjro Elizondo <alexelmtz@outlook.com>
2015-11-12 11:41:09 -0600

    Crear proyecto

---
+++ Proyecto_Integrador/Proyecto_Integrador/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Proyecto_Integrador
+//
+//  Created by Alex Elizondo on 11/12/15.
+//  Copyright © 2015 Alex Elizondo. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    return 0;
+}
