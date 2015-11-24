b79e45b6a5d5975bb2bfebe255232cfdedb69d9b
Alejandjro Elizondo <alexelmtz@outlook.com>
2015-11-23 21:18:08 -0600

    Avances

---
+++ Proyecto_Integrador_1.1/Proyecto_Integrador_1.1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Proyecto_Integrador_1.1
+//
+//  Created by Alex Elizondo on 11/17/15.
+//  Copyright © 2015 Alex Elizondo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
