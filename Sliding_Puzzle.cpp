bfd5ec42aea4fd1165daf5a5531a068e4697654f
Alejandjro Elizondo <alexelmtz@outlook.com>
2016-03-10 19:41:26 -0600

    Deleted old files

--- Sliding_Puzzle.cpp
+++ Sliding_Puzzle.cpp
@@ -1,13 +1,15 @@
 //	Alejandro Elizondo
+//	alexelmtz@outlook.com
+//	Linkedin: http://bit.ly/1pnePO4
 
 #include <iostream>
 #include <time.h>
 #include <string>
 
 using namespace std;
-// Displays the puzzle
-void Show (int Array[4][4]){
-	cout<<"•---•---•---•---•---•"<<endl;
+
+void Display (int Array[4][4]){
+	cout<< "•---•---•---•---•---•" <<endl;
 	for (int x = 0;x < 4;x++){
 		cout << "| ";
 		for (int y = 0;y < 4;y++){
@@ -35,19 +37,22 @@ void Generate(int Array[4][4]){
 		}
 }
 
+void Switch (int Array[4][4]){
+
+}
+
 int main() {
 	int Array[4][4], iNum;
 	srand(time(NULL));
 
-	cout << "\t" << "\t" << "Welcome to the game"<< endl << endl;
-    cout << "In order to win, order the numbers from 1 to 15, starting from the top-left corner. ";
+	cout << "\t" << "\t" << "\t" << "Welcome to the game"<< endl << endl;
+    cout << "In order to win, order the numbers from 1 to 15, starting from the top-left" << endl << "corner. ";
     cout << "The bottom-right corner of the puzzle should be 0 in order to win."<< endl << endl;
 	Generate(Array);
-	Show(Array);
+	Display(Array);
 	do{
 		cout << endl << "What number would you like to switch places with 0?" << endl;
 		cin >> iNum;
-		cout << iNum;
 	} while (iNum < 1 || iNum > 15);
 	return 0;
 }
